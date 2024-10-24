d1e0126bec0cebf357920bbee4c6e5fde0fb1679
shubham-kharkar <shubham.sskk@gmail.com>
2024-10-24 12:49:21 +0530

    Add files via upload

---
+++ Descriptive_Stats.R
@@ -0,0 +1,53 @@
+df <- datasets::airquality
+View(df)
+
+
+####Top 10 rows and last 10 rows
+
+head(df)
+head(df,1)
+tail(df)
+tail(df,25)
+
+###########Summary of the data#########
+
+summary(df)
+
+df$Ozone
+Ozone
+summary(df$Ozone)
+attach(df)
+Month
+Ozone
+
+###########DATA VISUALIZATION IN R##########
+#Scatter plot
+
+plot(x = Ozone, y = Temp)
+
+plot(Ozone,Wind, xlab = 'Ozone Levels',ylab = 'Wind Values',
+     main = 'Scatter Plot Between Ozone and Wind',
+     col = 'green',pch = 19)
+
+#Plot entire df
+plot(df)
+
+#Horizontal bar plot
+
+barplot(Month)
+unique(Month)
+fre <- table(Month)
+fre
+barplot(fre,col = 'darkred')
+
+#Histogram
+hist(Ozone)
+hist(Temp)
+hist(Wind)
+
+##Single box plot and stats
+boxplot(Ozone, col = 'blue')
+
+# Multiple box plots
+boxplot(df)
+
